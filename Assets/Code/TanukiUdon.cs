42565fdab308a77859224bf083de1e67921fde46
Damian Seals <>
2022-03-01 16:58:48 -0500

    Attempting to create looping teleporter forreal thiis time

--- Assets/Code/TanukiUdon.cs
+++ Assets/Code/TanukiUdon.cs
@@ -7,11 +7,16 @@ using UdonSharpEditor;
 
 public class TanukiUdonWindow : EditorWindow
 {
-    //Display settings for Pickup Tools dropdown
+    // Display settings for Pickup Tools dropdown
     bool showPickupTools = true;
-    string statusPickupTools = "Pickup Tools";
     bool pickupUsePhysics = true;
-    string pickupString = "Pick Up";
+    string pickupString = "Action Text";
+    string interactionString = "Hover Text";
+
+    // Display settings for Teleporter Tools dropdown
+    bool showTeleporterTools = true;
+    GameObject sourceTeleporter;
+    GameObject targetTeleporter;
 
     //Make the TanukiUdon window selection appear in Window toolbar tab
     [MenuItem("Window/TanukiUdon")]
@@ -28,10 +33,11 @@ public class TanukiUdonWindow : EditorWindow
         GUILayout.Label("TanukiUdon v0.0.1", EditorStyles.boldLabel);
 
         GUILayout.Space(25f);
+        GUIStyle style = EditorStyles.foldout;
+        style.fontStyle = FontStyle.Bold;
 
-
-        //Create the first dropdown group
-        showPickupTools = EditorGUILayout.Foldout(showPickupTools, statusPickupTools);
+        // Create the Pickup Tools dropdown group
+        showPickupTools = EditorGUILayout.Foldout(showPickupTools, "Pickup Tools", style);
         if (showPickupTools)
 		{
             //Add VRC Pickup and ObjectSync
@@ -44,7 +50,8 @@ public class TanukiUdonWindow : EditorWindow
 
 			}
             //Make textfield and let it update
-            pickupString = EditorGUILayout.TextField(pickupString, EditorStyles.textField);
+            pickupString = EditorGUILayout.TextField("Use Text", pickupString);
+            interactionString = EditorGUILayout.TextField("Interaction Text", interactionString);
             if (GUILayout.Button("Convert selected to Pickup"))
             {
                 Debug.Log("Converting to pickup...");
@@ -58,20 +65,40 @@ public class TanukiUdonWindow : EditorWindow
                     rigidbody.useGravity = pickupUsePhysics;
                     rigidbody.isKinematic = !pickupUsePhysics;
                     pickup.UseText = pickupString;
+                    pickup.InteractionText = interactionString;
 
                 }
-            }
+            }    
+        }
 
-            GUILayout.Space(25f);
-
+        // Create the Teleporter Tools dropdown group
+        showTeleporterTools = EditorGUILayout.Foldout(showTeleporterTools, "Teleporter Tools", style);
+        if (showTeleporterTools)
+		{
+            
             if (GUILayout.Button("Convert selected to Teleporter"))
 			{
                 foreach (GameObject obj in Selection.gameObjects)
 				{
-
                     obj.AddUdonSharpComponent<Tanuki.TanukiTeleporter>();
 				}
 			}
         }
+        sourceTeleporter = EditorGUILayout.ObjectField("Source Teleporter", sourceTeleporter, typeof(GameObject), true) as GameObject;
+        targetTeleporter = EditorGUILayout.ObjectField("Target Teleporter", targetTeleporter, typeof(GameObject), true) as GameObject;
+       
+        if (GUILayout.Button("Make To-From Teleporter Loop"))
+		{
+            sourceTeleporter.AddUdonSharpComponent<Tanuki.TanukiTeleporter>();
+            targetTeleporter.AddUdonSharpComponent<Tanuki.TanukiTeleporter>();
+
+            sourceTeleporter.GetComponent<Tanuki.TanukiTeleporter>().lerpOnRemote = true;
+            //st.lerpOnRemote = true;
+            //st.teleportTo = targetTeleporter.transform;
+            Debug.Log(sourceTeleporter);
+            //sourceTeleporter<Tanuki.TanukiTeleporter>().teleportTo = targetTeleporter;
+            //targetTeleporter.TanukiTeleporter.teleportTo = sourceTeleporter;
+            Debug.Log("Should have made loop");
+		}
     }
 }
