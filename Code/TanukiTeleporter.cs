abd7799ba70fbca95d864655e2f9b9ffd8136fbc
itwangotwango@gmail.com <>
2022-03-03 22:22:56 -0500

    Sprint 3.1

---
+++ Code/TanukiTeleporter.cs
@@ -0,0 +1,26 @@
+﻿
+/* TanukiUdon Teleporter */
+/* Based on JetDog Teleport_Event */
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Tanuki
+{
+    public class TanukiTeleporter : UdonSharpBehaviour
+    {
+        public Transform teleportTo;
+        public VRC_SceneDescriptor.SpawnOrientation teleportOrientation = VRC_SceneDescriptor.SpawnOrientation.AlignPlayerWithSpawnPoint;
+        public bool lerpOnRemote = false;
+
+        public virtual void OnPlayerTriggerEnter(VRC.SDKBase.VRCPlayerApi player)
+		{
+            if (player.isLocal)
+			{
+                player.TeleportTo(teleportTo.position, teleportTo.rotation, teleportOrientation, lerpOnRemote);
+			}
+		}
+    }
+}
