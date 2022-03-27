5109f6e1a549160e65ada7b31e62c310b5cdb9ff
dseals3@vols.utk.edu <>
2022-03-27 13:24:37 -0400

    Refactoring completed, toolbar added and new files created. Left comments in code for potential new issues to add to github

---
+++ Assets/Editor/Tanuki-Whatever.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using VRC.SDK3.Components;
+using VRC.Udon;
+using UdonSharp;
+using UdonSharpEditor;
+
+public partial class TanukiUdonWindow : EditorWindow
+{
+    // I honestly cannot remember what the third thing it what that you
+    // wanted so whatever is what you get
+    void ShowWhatever()
+    {
+        EditorGUILayout.BeginVertical();
+
+        GUILayout.Label("Insert Whatever Here");
+
+        EditorGUILayout.EndVertical();
+    }
+
+}
